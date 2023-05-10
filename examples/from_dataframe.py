271de14665ab171ae1f4c3cdebfd03d2f4e79a8a
jkf87 <jjoongoo@gmail.com>
2023-05-10 11:21:53 +0900

    conver to korean

--- examples/from_dataframe.py
+++ examples/from_dataframe.py
@@ -10,6 +10,6 @@ df = pd.DataFrame(dataframe)
 
 llm = OpenAI()
 pandas_ai = PandasAI(llm, verbose=True, conversational=False)
-response = pandas_ai.run(df, "Calculate the sum of the gdp of north american countries")
+response = pandas_ai.run(df, "북아메리카 국가의 GDP 합계 계산하기")
 print(response)
 # Output: 20901884461056
